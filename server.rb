6245c6c4f5732de70bb3ca882d2b87f41656e97f
kyjoya <kyleejacker@gmail.com>
2018-01-09 11:46:01 -0500

    app setup

---
+++ server.rb
@@ -0,0 +1,19 @@
+require "sinatra"
+require "sinatra/json"
+require "json"
+require "pry"
+
+# You should not need to change the code in this file
+
+set :bind, '0.0.0.0'  # bind to all interfaces
+set :public_folder, File.join(File.dirname(__FILE__), "public")
+set :views, File.dirname(__FILE__) + "/app/views"
+
+Dir[File.join(File.dirname(__FILE__), 'app', '**', '*.rb')].each do |file|
+  require file
+  also_reload file
+end
+
+get '/' do
+  erb :index
+end
